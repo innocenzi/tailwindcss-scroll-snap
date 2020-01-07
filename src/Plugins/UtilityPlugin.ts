8dde78bb78ccc2d6c93d6303887b79b2718a0d76
Haew <hi@haew.xyz>
2020-01-07 16:35:12 +0100

    Fixed comments

--- src/Plugins/UtilityPlugin.ts
+++ src/Plugins/UtilityPlugin.ts
@@ -7,6 +7,8 @@ import _ from 'lodash';
  * @export
  * @param {string} property The Tailwind config key, which will also be the CSS property. Should be in the CSS-in-JS format.
  * @param {*} config A map of class names => property values.
+ *                   The property values can be a string tuple where the first value would be the property name,
+ *                   and the second value the property value.
  * @param {TailwindPluginHelpers} helpers The Tailwind plugin helpers object.
  */
 export default function(property: string, config: any, { e, theme, variants, addUtilities }: TailwindPluginHelpers) {
@@ -18,10 +20,24 @@ export default function(property: string, config: any, { e, theme, variants, add
   );
 }
 
+/**
+ * Parses the property name from the value to detect tuples.
+ *
+ * @param {string} property
+ * @param {(string | [string, string])} value
+ * @returns
+ */
 function getPropertyName(property: string, value: string | [string, string]) {
   return Array.isArray(value) ? value[0] : property;
 }
 
+/**
+ * Parses the actual value name from the value variable to detect tuples.
+ *
+ * @param {string} property
+ * @param {(string | [string, string])} value
+ * @returns
+ */
 function getValue(value: string | [string, string]) {
   return Array.isArray(value) ? value[1] : value;
 }
